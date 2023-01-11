e470ba24ee4c2ff3abccf254f95ea981651466be
phil1366 <dslame@gmail.com>
2023-01-11 09:27:14 +0100

    Projet 5 Kanap

--- front/js/product.js
+++ front/js/product.js
@@ -1,33 +1,7 @@
-/*let section = document.getElementByid('item');
 
-let params = new URL (document.location).searchParams;
-let id = params.get('id')
-
-console.log(id);
-
-const display = (produit) => {
-
-
-let article = document.createElement('article');
-
-let img = document.createElement('img'); 
-img.setAttribute('src',`${produit.imageUrl}`);
-img.setAttribute('alt',`${produit.altTxt}`);
-}
-
-let h3 = document.createElement('h1');
-     h3.textContent =`${produit.name}`;
-     h3.classList.add("productName"); */
-
-     /*let img = document.createElement('img'); 
-        img.setAttribute('src',`${produit.imageUrl}`);
-        img.setAttribute('alt',`${produit.altTxt}`);*/
 
      const displayProduct = (product) => {      
         
-        /*let image = document.querySelector(".item__img img");
-        image.setAttribute("src", product.imageUrl);
-        image.setAttribute("alt", product.altTxt);*/
 
         const title = document.getElementById("title");
         title.textContent = product.name;
@@ -63,3 +37,45 @@ let h3 = document.createElement('h1');
       const productId = searchParams.get("id");
       fetchProduct(productId);
   
+
+      /*const confirmOrder = () => {
+        // récupération des éléments du formulaire
+        const form = document.getElementById("order-form");
+        const formData = new FormData(form);
+    
+        // envoi de la demande de confirmation de commande au serveur
+        fetch("http://localhost:3000/api/orders/confirm", {
+            method: "POST",
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            // vérification de la réponse du serveur
+            if (data.success) {
+                // mise à jour de la page pour afficher un message de succès
+                const confirmationMessage = document.getElementById("confirmation-message");
+                confirmationMessage.textContent = "Votre commande a été confirmée avec succès ! Votre numéro de commande est : " + data.orderId;
+                confirmationMessage.style.display = "block";
+            } else {
+                // affichage d'un message d'erreur
+                alert("Une erreur est survenue lors de la confirmation de votre commande. Veuillez réessayer plus tard.");
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            alert("Une erreur est survenue lors de la confirmation de votre commande. Veuillez réessayer plus tard.");
+        });
+    };
+    
+    // ajout d'un écouteur d'événement pour l'envoi du formulaire
+    const form = document.getElementById("order-form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        confirmOrder();
+    });
+    Il faudra ajouter un formulaire HTML pour recupérer les données, et un bouton ou un élément HTML qui écoutera l'événement submit pour ensuite appeler la fonction confirmOrder() pour envoyer les données. Il faudrait également un élément pour afficher un message de confirmation de la commande.
+    
+    
+    
+    
+    
