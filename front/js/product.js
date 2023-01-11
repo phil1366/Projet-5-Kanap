59045ba3ab1758dacbbbba5a14d2c092329d56fe
phil1366 <dslame@gmail.com>
2023-01-11 09:29:20 +0100

    travail sur la page product.js

--- front/js/product.js
+++ front/js/product.js
@@ -38,42 +38,7 @@
       fetchProduct(productId);
   
 
-      /*const confirmOrder = () => {
-        // récupération des éléments du formulaire
-        const form = document.getElementById("order-form");
-        const formData = new FormData(form);
-    
-        // envoi de la demande de confirmation de commande au serveur
-        fetch("http://localhost:3000/api/orders/confirm", {
-            method: "POST",
-            body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-            // vérification de la réponse du serveur
-            if (data.success) {
-                // mise à jour de la page pour afficher un message de succès
-                const confirmationMessage = document.getElementById("confirmation-message");
-                confirmationMessage.textContent = "Votre commande a été confirmée avec succès ! Votre numéro de commande est : " + data.orderId;
-                confirmationMessage.style.display = "block";
-            } else {
-                // affichage d'un message d'erreur
-                alert("Une erreur est survenue lors de la confirmation de votre commande. Veuillez réessayer plus tard.");
-            }
-        })
-        .catch(error => {
-            console.error(error);
-            alert("Une erreur est survenue lors de la confirmation de votre commande. Veuillez réessayer plus tard.");
-        });
-    };
-    
-    // ajout d'un écouteur d'événement pour l'envoi du formulaire
-    const form = document.getElementById("order-form");
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        confirmOrder();
-    });
-    Il faudra ajouter un formulaire HTML pour recupérer les données, et un bouton ou un élément HTML qui écoutera l'événement submit pour ensuite appeler la fonction confirmOrder() pour envoyer les données. Il faudrait également un élément pour afficher un message de confirmation de la commande.
+      
     
     
     
